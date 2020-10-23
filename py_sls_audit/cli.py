279a5ca3ec51cfd84261e10a7e4d71c14a83de0d
Jacek Nosal <jacek.nosal@outlook.com>
2020-10-23 11:05:57 +0200

    List lambdas

--- py_sls_audit/cli.py
+++ py_sls_audit/cli.py
@@ -1,2 +1,26 @@
-def run():
-    print("INIT")
+import boto3
+import botocore.exceptions
+import click
+
+import sys
+
+
+def list_functions(session):
+    client = session.client('lambda')
+
+    response = client.list_functions()
+    for fun in response.get('Functions'):
+        print(fun.get('FunctionName'))
+
+
+@click.command()
+@click.option('-p', '--profile', help='AWS profile to use.', required=True)
+@click.option('-r', '--region', help='AWS region.', default='eu-west-1')
+def run(profile, region):
+    try:
+        session = boto3.Session(profile_name=profile, region_name=region)
+    except botocore.exceptions.ProfileNotFound:
+        print(f"Could not load profile {profile}")
+        sys.exit(1)
+
+    list_functions(session)
